d0923a2db46ac0392a7993d3d9d4065358bc53c6
CarlosDaniel0293 <carlos.mendoza.co@tecsup.edu.pe>
2024-04-06 21:03:14 -0500

    segundo commit lab04

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const productos = [
     { id: 1, nombre: 'Producto 1', precio: 10 },
     { id: 2, nombre: 'Producto 2', precio: 20 },
@@ -8,7 +10,41 @@ const productos = [
   ];
 
 app.get('/productos', (req, res) => {
-res.json(productos);
+  res.json(productos);
+});
+
+app.post('/productos', (req, res) => {
+    const nuevoProducto = req.body;
+    productos.push(nuevoProducto);
+    res.json(nuevoProducto);
+});
+
+app.put('/productos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const productoActualizado = req.body;
+  
+    const producto = productos.find(p => p.id === id);
+    if (!producto) {
+      return res.status(404).json({ error: 'Producto no encontrado' });
+    }
+  
+    producto.nombre = productoActualizado.nombre;
+    producto.precio = productoActualizado.precio;
+  
+    res.json(producto);
+});
+
+app.delete('/productos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const index = productos.findIndex(p => p.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: 'Producto no encontrado' });
+  }
+
+  productos.splice(index, 1);
+
+  res.json({ mensaje: 'Producto eliminado' });
 });
 
 const port = process.env.PORT || 3000;
